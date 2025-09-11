52d4c245228cbb44c7a71790e37047f19c098ca9
Aneesh Kumar <53368806+wowitsaneesh@users.noreply.github.com>
2025-09-11 20:20:34 +0800

    added studentIDs

--- conv_test.c
+++ conv_test.c
@@ -1,3 +1,6 @@
+// STUDENT NAME: JALIL INAYAT-HUSSAIN , STUDENT ID : 22751096
+// STUDENT NAME: ANEESH KUMAR BANDARI , STUDENT ID : 24553634
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>     
